eafb07d26976be0db2325fa742d6974163f775b3
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2023-10-23 12:47:32 +0900

    feat: remove bin/setup

--- README.md
+++ README.md
@@ -4,7 +4,6 @@
 
 ```bash
 cp .env.sample .env
-bin/setup
 ```
 
 and place your jwk.json to `config/credentials/jwk.json`
--- config/initializer/active_record.rb
+++ config/initializer/active_record.rb
@@ -8,3 +8,25 @@ ActiveRecord::Base.establish_connection(
   adapter:   'sqlite3',
   database:  "#{__dir__}/../../db/regtest.db"
 )
+
+unless ActiveRecord::Base.connection.table_exists?(:utxos)
+  ActiveRecord::Migration.create_table :utxos, if_not_exists: true do |t|
+    t.string :txid, index: true
+    t.integer :vout
+    t.string :token
+    t.integer :value
+    t.string :script_pubkey
+    t.string :out_point, index: { unique: true }
+
+    t.timestamps
+  end
+end
+
+unless ActiveRecord::Base.connection.table_exists?(:infos)
+  ActiveRecord::Migration.create_table :infos, if_not_exists: true do |t|
+    t.string :key, index: { unique: true }
+    t.string :value
+
+    t.timestamps
+  end
+end

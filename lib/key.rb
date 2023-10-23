d8289b12506adc18ee8b629daeb1f0b6fe9230ce
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2023-10-23 12:26:28 +0900

    refactor: rename Key#from_jwk

--- lib/key.rb
+++ lib/key.rb
@@ -2,7 +2,7 @@
 
 class Key
   # @params hash jwk_hash
-  def self.generate_from_jwk(jwk_hash = nil)
+  def self.from_jwk(jwk_hash = nil)
     jwk = JSON::JWK.new(jwk_hash || load_from_json_file)
     Tapyrus::Key.new(priv_key: jwk.to_key.private_key.to_s(16).downcase.encode('US-ASCII'), key_type: 0)
   end

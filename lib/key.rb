31686366a4eccf9e27ee1bcaaf5b5660a555296c
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2023-10-25 14:59:56 +0900

    feat: payment cli

--- lib/key.rb
+++ lib/key.rb
@@ -5,7 +5,12 @@ module FCSWallet
     # @params hash jwk_hash
     def self.from_jwk(jwk_hash = nil)
       jwk = JSON::JWK.new(jwk_hash || load_from_json_file)
-      Tapyrus::Key.new(priv_key: jwk.to_key.private_key.to_s(16).downcase.encode('US-ASCII'), key_type: 0)
+      key = jwk.to_key
+      if key.private_key.nil?
+        Tapyrus::Key.new(pubkey: key.public_key.to_bn.to_s(16).downcase.encode('US-ASCII'), key_type: 0)
+      else
+        Tapyrus::Key.new(priv_key: key.private_key.to_s(16).downcase.encode('US-ASCII'), key_type: 0)
+      end
     end
 
     private
---
+++ lib/vc.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module FCSWallet
+  class VC
+    attr_accessor :jwt
+
+    def initialize(jwt)
+      @jwt = jwt
+    end
+
+    def verify
+      response = Net::HTTP.post(
+        URI("#{ENV['DID_SERVICE_SCHEMA']}://#{ENV['DID_SERVICE_HOST']}:#{ENV['DID_SERVICE_PORT']}/vc/verify"),
+        { "vcJwt": self.jwt }.to_json,
+        'Content-Type' => 'application/json'
+      )
+      body = JSON.parse(response.body)
+
+      {
+        verify: body['verified'],
+        vc: body['vc'],
+        issuer_pubkey_jwk: body['issuerDid']['didDocument']['verificationMethod'][0]['publicKeyJwk'],
+        holder_pubkey_jwk: body['holderDid']['didDocument']['verificationMethod'][0]['publicKeyJwk']
+      }
+    end
+  end
+end
